e0fec2397665e8bd68f6f90c19195c5b00304f7a
houssein <saban.houssein1@gmail.com>
2025-04-24 15:13:31 +0200

    fix unset underflow error

--- sysdeps/ieee754/dbl-64/e_atan2.c
+++ sysdeps/ieee754/dbl-64/e_atan2.c
@@ -231,6 +231,8 @@ atan2_accurate (double y, double x)
       if (underflow)
         errno = ERANGE; // underflow
 #endif
+      if (!fetestexcept (FE_UNDERFLOW))
+        fesetexceptflag (&flag, FE_UNDERFLOW);
       return t;
     }
     res = (y > 0) ? PI_H + PI_L : -PI_H - PI_L;
@@ -249,6 +251,8 @@ atan2_accurate (double y, double x)
         if (underflow)
           errno = ERANGE; // underflow
 #endif
+        if (!fetestexcept (FE_UNDERFLOW))
+          fesetexceptflag (&flag, FE_UNDERFLOW);
         return __builtin_fma (t, -0x1p-54, t);
       }
       /* Now |y| < 2^-969, since x >= 2^-1074, then t <= 2^105, thus we can
@@ -260,6 +264,8 @@ atan2_accurate (double y, double x)
         if (underflow)
           errno = ERANGE; // underflow
 #endif
+        if (!fetestexcept (FE_UNDERFLOW))
+          fesetexceptflag (&flag, FE_UNDERFLOW);
         return res;
       }
     }
@@ -388,6 +394,8 @@ atan2_accurate (double y, double x)
 #endif
   fesetexceptflag (&flag, FE_OVERFLOW); // restore overflow flag
   feraiseexcept (FE_INEXACT); // always inexact
+  if(!fetestexcept (FE_UNDERFLOW))
+    fesetexceptflag (&flag, FE_UNDERFLOW);
   return res;
 }
 
