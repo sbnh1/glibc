215371ffb892fd8a290175813885df77a523f00a
houssein <saban.houssein1@gmail.com>
2025-05-13 11:38:22 +0200

    fix errors

--- sysdeps/ieee754/dbl-64/s_sin.c
+++ sysdeps/ieee754/dbl-64/s_sin.c
@@ -32,6 +32,7 @@
 /*                                                                          */
 /****************************************************************************/
 
+/* Based on commit 10e79b1f */
 
 #include <errno.h>
 #include <float.h>
@@ -2207,9 +2208,14 @@ __sin (double x)
   if (__builtin_expect (e == 0x7ff, 0)) /* NaN, +Inf and -Inf. */
     {
 #ifdef CORE_MATH_SUPPORT_ERRNO
-      if ((t.u << 1) == 0x7ffull<<53) // Inf
+      if ((t.u << 1) == 0x7ffull<<53){ // Inf
         errno = EDOM;
+        return 0.0 / 0.0;
+      }
 #endif
+      if ((t.u << 1) != 0x7ff8ull<<49){
+        return 0.0 / 0.0;
+      }
       t.u = ~0ull;
       return t.f;
     }
