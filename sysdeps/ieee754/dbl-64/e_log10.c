551c3be38105b6a543a469ce15cc7ffd92331cbd
houssein <saban.houssein1@gmail.com>
2025-05-13 15:28:05 +0200

    fix qnan values issue

--- sysdeps/ieee754/dbl-64/e_log10.c
+++ sysdeps/ieee754/dbl-64/e_log10.c
@@ -713,6 +713,10 @@ __ieee754_log10 (double x)
   int e = (v.u >> 52) - 0x3ff;
   if (e >= 0x400 || e == -0x3ff) /* x <= 0 or NaN/Inf or subnormal */
   {
+    static const d64u64 minf = {.u = 0xffful << 52};
+    if (e == 0x400 || (e == 0xc00 && x != minf.f)) /* +Inf or NaN */
+      return x + x;
+
     if (x <= 0.0)
     {
       /* log10(x<0) is NaN, f(+/-0) is -Inf and raises DivByZero */
@@ -729,8 +733,6 @@ __ieee754_log10 (double x)
         return 1.0 / -0.0;
       }
     }
-    if (e == 0x400 || e == 0xc00) /* +Inf or NaN */
-      return x + x;
     if (e == -0x3ff) /* subnormal */
     {
       v.f *= 0x1p52;
