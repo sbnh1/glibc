e3b500a04c96c58bd4ee704a62a84b0d5a89a3c5
houssein <saban.houssein1@gmail.com>
2025-05-20 14:52:49 +0200

    fix error for +-Inf values

--- sysdeps/ieee754/dbl-64/e_sinh.c
+++ sysdeps/ieee754/dbl-64/e_sinh.c
@@ -290,6 +290,7 @@ __ieee754_sinh (double x)
     if(lb == ub) return lb;
     return as_sinh_zero(x);
   }
+  if(aix==0x7ff0000000000000ull) return x; // inf
   int64_t il = ((u64)jt.u<<14)>>40, jl = -il;
   int64_t i1 = il&0x3f, i0 = (il>>6)&0x3f, ie = il>>12;
   int64_t j1 = jl&0x3f, j0 = (jl>>6)&0x3f, je = jl>>12;
@@ -306,7 +307,7 @@ __ieee754_sinh (double x)
   if(__builtin_expect(aix>0x4014000000000000ull, 0)){ // |x| > 5
     if(__builtin_expect(aix>0x40425e4f7b2737faull, 0)){ // |x| >~ 36.736801
       if(__builtin_expect(aix>0x408633ce8fb9f87dull, 0)){ // |x| >~ 710.47586
-	if(aix>=0x7ff0000000000000ull) return x + x; // nan
+        if(aix>0x7ff0000000000000ull) return x + x; // nan
 #ifdef CORE_MATH_SUPPORT_ERRNO
   errno = ERANGE;
 #endif
