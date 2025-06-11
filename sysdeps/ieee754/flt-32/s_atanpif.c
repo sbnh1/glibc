1bb7a73d8b377f735d6738af12c3cd98a5809219
houssein <saban.houssein1@gmail.com>
2025-06-11 09:43:25 +0200

    Integration of the singl-precision atanpif function

--- sysdeps/ieee754/flt-32/s_atanpif.c
+++ sysdeps/ieee754/flt-32/s_atanpif.c
@@ -25,85 +25,88 @@ SOFTWARE.
 
 */
 
-#include <errno.h>
-#include <math.h>
+/* Based on commit 03c15350 */
+
 #include <stdint.h>
+#include <errno.h>
 #include <libm-alias-float.h>
-#include "math_config.h"
+
+// Warning: clang also defines __GNUC__
+#if defined(__GNUC__) && !defined(__clang__)
+#pragma GCC diagnostic ignored "-Wunknown-pragmas"
+#endif
+
+#pragma STDC FENV_ACCESS ON
+
+#define CORE_MATH_SUPPORT_ERRNO
+
+typedef union {float f; uint32_t u;} b32u32_u;
 
 float
 __atanpif (float x)
 {
-  uint32_t t = asuint (x);
-  int32_t e = (t >> 23) & 0xff;
-  bool gt = e >= 127;
-  if (__glibc_unlikely (e > 127 + 24))
-    {
-      float f = copysignf (0.5f, x);
-      if (__glibc_unlikely (e == 0xff))
-	{
-	  if (t << 9)
-	    return x + x; /* nan */
-	  return f;	  /* inf */
-	}
-      /* Warning: 0x1.45f306p-2f / x underflows for |x| >= 0x1.45f306p+124 */
-      if (fabsf (x) >= 0x1.45f306p+124f)
-	return f - copysign (0x1p-26f, x);
-      else
-	return f - 0x1.45f306p-2f / x;
+  b32u32_u t = {.f = x};
+  int32_t e = (t.u>>23)&0xff, gt = e>=127;
+  if(__builtin_expect(e>127+24, 0)) { // |x| >= 2^25
+    float f = __builtin_copysignf(0.5f, x);
+    if(__builtin_expect(e==0xff, 0)) {
+      if(t.u<<9) return x + x; // nan
+      return f; // inf
     }
+    // Warning: 0x1.45f306p-2f / x underflows for |x| >= 0x1.45f306p+124
+    if (__builtin_fabsf (x) >= 0x1.45f306p+124f)
+      return f - __builtin_copysignf(0x1p-26f, x);
+    else
+      return f - 0x1.45f306p-2f / x;
+  }
   double z = x;
-  if (__glibc_unlikely (e < 127 - 13))
-    {
-      double sx = z * 0x1.45f306dc9c883p-2;
-      if (__glibc_unlikely (e < 127 - 25))
-	{
-	  float rsx = sx;
-	  if (x != 0 && rsx == 0)
-	    __set_errno (ERANGE);
-	  return rsx;
-	}
-      return sx - (0x1.5555555555555p-2 * sx) * (x * x);
+  if (__builtin_expect(e<127-13, 0)){ // |x| < 2^-13
+    double sx = z*0x1.45f306dc9c883p-2;
+    if (__builtin_expect(e<127-25, 0)) { // |x| < 2^-25
+#ifdef CORE_MATH_SUPPORT_ERRNO
+    /* For rounding towards zero, the largest positive number for which there
+       is underflow is 0x1.921fb4p-125.
+       For rounding to nearest, it is 0x1.921fb4p-125 too (although the result
+       is 0x1p-126).
+       For rounding upwards, it is 0x1.921fb2p-125. */
+#define THRESHOLD 0x1.fffffe632357dp-127
+#define C0 0x1.45f306dc9c882p-2
+      if (x != 0.0f && (__builtin_fabsf (x) <= 0x1.921fb2p-125f ||
+                        (__builtin_fabsf (x) == 0x1.921fb4p-125f &&
+                         __builtin_fabs (z * C0) <= THRESHOLD)))
+      errno = ERANGE; // underflow
+#endif
+      return sx;
     }
-  uint32_t ax = t & (~0u >> 1);
-  if (__glibc_unlikely (ax == 0x3fa267ddu))
-    return copysignf (0x1.267004p-2f, x) - copysignf (0x1p-55f, x);
-  if (__glibc_unlikely (ax == 0x3f693531u))
-    return copysignf (0x1.e1a662p-3f, x) + copysignf (0x1p-28f, x);
-  if (__glibc_unlikely (ax == 0x3f800000u))
-    return copysignf (0x1p-2f, x);
-  if (gt)
-    z = 1 / z;
-  double z2 = z * z;
-  double z4 = z2 * z2;
-  double z8 = z4 * z4;
+    return sx - (0x1.5555555555555p-2*sx)*(x*x);
+  }
+  uint32_t ax = t.u&(~0u>>1);
+  if(__builtin_expect(ax == 0x3fa267ddu, 0)) return __builtin_copysignf(0x1.267004p-2f,x) - __builtin_copysignf(0x1p-55f,x);
+  if(__builtin_expect(ax == 0x3f693531u, 0)) return __builtin_copysignf(0x1.e1a662p-3f,x) + __builtin_copysignf(0x1p-28f,x);
+  if(__builtin_expect(ax == 0x3f800000u, 0)) return __builtin_copysignf(0x1p-2f,x);
+  if(gt) z = 1/z;
+  double z2 = z*z, z4 = z2*z2, z8 = z4*z4;
   static const double cn[] =
-    {
-      0x1.45f306dc9c882p-2, 0x1.733b561bc23d5p-1, 0x1.28d9805bdfbf2p-1,
-      0x1.8c3ba966ae287p-3, 0x1.94a7f81ee634bp-6, 0x1.a6bbf6127a6dfp-11
-    };
+    {0x1.45f306dc9c882p-2, 0x1.733b561bc23d5p-1, 0x1.28d9805bdfbf2p-1,
+     0x1.8c3ba966ae287p-3, 0x1.94a7f81ee634bp-6, 0x1.a6bbf6127a6dfp-11};
   static const double cd[] =
-    {
-      0x1p+0,               0x1.4e3b3ecc2518fp+1, 0x1.3ef4a360ff063p+1,
-      0x1.0f1dc55bad551p+0, 0x1.8da0fecc018a4p-3, 0x1.8fa87803776bfp-7,
-      0x1.dadf2ca0acb43p-14
-    };
-  double cn0 = cn[0] + z2 * cn[1];
-  double cn2 = cn[2] + z2 * cn[3];
-  double cn4 = cn[4] + z2 * cn[5];
-  cn0 += z4 * cn2;
-  cn0 += z8 * cn4;
+    {0x1p+0, 0x1.4e3b3ecc2518fp+1, 0x1.3ef4a360ff063p+1, 0x1.0f1dc55bad551p+0,
+     0x1.8da0fecc018a4p-3, 0x1.8fa87803776bfp-7, 0x1.dadf2ca0acb43p-14};
+  double cn0 = cn[0] + z2*cn[1];
+  double cn2 = cn[2] + z2*cn[3];
+  double cn4 = cn[4] + z2*cn[5];
+  cn0 += z4*cn2;
+  cn0 += z8*cn4;
   cn0 *= z;
-  double cd0 = cd[0] + z2 * cd[1];
-  double cd2 = cd[2] + z2 * cd[3];
-  double cd4 = cd[4] + z2 * cd[5];
+  double cd0 = cd[0] + z2*cd[1];
+  double cd2 = cd[2] + z2*cd[3];
+  double cd4 = cd[4] + z2*cd[5];
   double cd6 = cd[6];
-  cd0 += z4 * cd2;
-  cd4 += z4 * cd6;
-  cd0 += z8 * cd4;
-  double r = cn0 / cd0;
-  if (gt)
-    r = copysign (0.5, z) - r;
+  cd0 += z4*cd2;
+  cd4 += z4*cd6;
+  cd0 += z8*cd4;
+  double r = cn0/cd0;
+  if (gt) r = __builtin_copysign(0.5, z) - r;
   return r;
 }
 libm_alias_float (__atanpi, atanpi)
