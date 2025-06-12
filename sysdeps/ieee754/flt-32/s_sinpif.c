95c5d9885358d6b787d9bf24fa33dcb65c88f253
houssein <saban.houssein1@gmail.com>
2025-06-12 15:38:11 +0200

    Integration of the single-precision sinpi function

--- sysdeps/ieee754/flt-32/s_sinpif.c
+++ sysdeps/ieee754/flt-32/s_sinpif.c
@@ -24,57 +24,100 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-#include <math.h>
+/* Based on commit 03c15350 */
+
 #include <stdint.h>
 #include <errno.h>
 #include <fenv.h>
 #include <libm-alias-float.h>
-#include "s_sincospif_data.h"
-#include "math_config.h"
+
+// Warning: clang also defines __GNUC__
+#if defined(__GNUC__) && !defined(__clang__)
+#pragma GCC diagnostic ignored "-Wunknown-pragmas"
+#endif
+
+#pragma STDC FENV_ACCESS ON
+
+#define CORE_MATH_SUPPORT_ERRNO
+
+typedef union {float f; uint32_t u;} b32u32_u;
 
 float
 __sinpif (float x)
 {
-  uint32_t ix = asuint (x);
-  int32_t e = (ix >> 23) & 0xff;
-  if (__glibc_unlikely (e == 0xff))
-    {
-      if (!(ix << 9))
-        return __math_invalidf (x);
-      return x + x; /* nan */
-    }
-  int32_t m = (ix & ~0u >> 9) | 1 << 23, sgn = ix;
-  sgn >>= 31;
-  m = (m ^ sgn) - sgn;
-  int32_t s = 143 - e;
-  if (__glibc_unlikely (s < 0))
-    {
-      if (__glibc_unlikely (s < -6))
-	return copysignf (0.0f, x);
-      int32_t iq = (uint32_t)m << (-s - 1);
-      iq &= 127;
-      if (iq == 0 || iq == 64)
-	return copysignf (0.0f, x);
-      return S[iq];
-    }
-  else if (__glibc_unlikely (s > 30))
-    {
-      double z = x, z2 = z * z;
-      return z * (0x1.921fb54442d18p+1 + z2 * (-0x1.4abbce625be53p+2));
-    }
-  int32_t si = 25 - s;
-  if (__glibc_unlikely (si >= 0 && ((uint32_t)m << si) == 0))
-    return copysignf (0.0f, x);
+  static const double sn[] = { 0x1.921fb54442d0fp-37, -0x1.4abbce6102b94p-112,  0x1.4669fa3c58463p-189};
+  static const double cn[] = {-0x1.3bd3cc9be45cfp-74, 0x1.03c1f08088742p-150, -0x1.55d1e5eff55a5p-228};
+  static const double S[] =
+    {0x0p+0, 0x1.91f65f10dd814p-5, 0x1.917a6bc29b42cp-4, 0x1.2c8106e8e613ap-3, 0x1.8f8b83c69a60bp-3, 0x1.f19f97b215f1bp-3,
+    0x1.294062ed59f06p-2, 0x1.58f9a75ab1fddp-2, 0x1.87de2a6aea963p-2, 0x1.b5d1009e15ccp-2, 0x1.e2b5d3806f63bp-2,
+    0x1.073879922ffeep-1, 0x1.1c73b39ae68c8p-1, 0x1.30ff7fce17035p-1, 0x1.44cf325091dd6p-1, 0x1.57d69348cecap-1,
+    0x1.6a09e667f3bcdp-1, 0x1.7b5df226aafafp-1, 0x1.8bc806b151741p-1, 0x1.9b3e047f38741p-1, 0x1.a9b66290ea1a3p-1,
+    0x1.b728345196e3ep-1, 0x1.c38b2f180bdb1p-1, 0x1.ced7af43cc773p-1, 0x1.d906bcf328d46p-1, 0x1.e212104f686e5p-1,
+    0x1.e9f4156c62ddap-1, 0x1.f0a7efb9230d7p-1, 0x1.f6297cff75cbp-1, 0x1.fa7557f08a517p-1, 0x1.fd88da3d12526p-1,
+    0x1.ff621e3796d7ep-1,0x1p+0, 0x1.ff621e3796d7ep-1, 0x1.fd88da3d12526p-1, 0x1.fa7557f08a517p-1, 0x1.f6297cff75cbp-1,
+    0x1.f0a7efb9230d7p-1, 0x1.e9f4156c62ddap-1, 0x1.e212104f686e5p-1, 0x1.d906bcf328d46p-1, 0x1.ced7af43cc773p-1,
+    0x1.c38b2f180bdb1p-1, 0x1.b728345196e3ep-1, 0x1.a9b66290ea1a3p-1, 0x1.9b3e047f38741p-1, 0x1.8bc806b151741p-1,
+    0x1.7b5df226aafafp-1, 0x1.6a09e667f3bcdp-1, 0x1.57d69348cecap-1, 0x1.44cf325091dd6p-1, 0x1.30ff7fce17035p-1,
+    0x1.1c73b39ae68c8p-1, 0x1.073879922ffeep-1, 0x1.e2b5d3806f63bp-2, 0x1.b5d1009e15ccp-2, 0x1.87de2a6aea963p-2,
+    0x1.58f9a75ab1fddp-2, 0x1.294062ed59f06p-2, 0x1.f19f97b215f1bp-3, 0x1.8f8b83c69a60bp-3, 0x1.2c8106e8e613ap-3,
+    0x1.917a6bc29b42cp-4, 0x1.91f65f10dd814p-5,0x0p+0, -0x1.91f65f10dd814p-5, -0x1.917a6bc29b42cp-4, -0x1.2c8106e8e613ap-3,
+    -0x1.8f8b83c69a60bp-3, -0x1.f19f97b215f1bp-3, -0x1.294062ed59f06p-2, -0x1.58f9a75ab1fddp-2, -0x1.87de2a6aea963p-2,
+    -0x1.b5d1009e15ccp-2, -0x1.e2b5d3806f63bp-2, -0x1.073879922ffeep-1, -0x1.1c73b39ae68c8p-1, -0x1.30ff7fce17035p-1,
+    -0x1.44cf325091dd6p-1, -0x1.57d69348cecap-1, -0x1.6a09e667f3bcdp-1, -0x1.7b5df226aafafp-1, -0x1.8bc806b151741p-1,
+    -0x1.9b3e047f38741p-1, -0x1.a9b66290ea1a3p-1, -0x1.b728345196e3ep-1, -0x1.c38b2f180bdb1p-1, -0x1.ced7af43cc773p-1,
+    -0x1.d906bcf328d46p-1, -0x1.e212104f686e5p-1, -0x1.e9f4156c62ddap-1, -0x1.f0a7efb9230d7p-1, -0x1.f6297cff75cbp-1,
+    -0x1.fa7557f08a517p-1, -0x1.fd88da3d12526p-1, -0x1.ff621e3796d7ep-1,-0x1p+0, -0x1.ff621e3796d7ep-1,
+    -0x1.fd88da3d12526p-1, -0x1.fa7557f08a517p-1, -0x1.f6297cff75cbp-1, -0x1.f0a7efb9230d7p-1, -0x1.e9f4156c62ddap-1,
+    -0x1.e212104f686e5p-1, -0x1.d906bcf328d46p-1, -0x1.ced7af43cc773p-1, -0x1.c38b2f180bdb1p-1, -0x1.b728345196e3ep-1,
+    -0x1.a9b66290ea1a3p-1, -0x1.9b3e047f38741p-1, -0x1.8bc806b151741p-1, -0x1.7b5df226aafafp-1, -0x1.6a09e667f3bcdp-1,
+    -0x1.57d69348cecap-1, -0x1.44cf325091dd6p-1, -0x1.30ff7fce17035p-1, -0x1.1c73b39ae68c8p-1, -0x1.073879922ffeep-1,
+    -0x1.e2b5d3806f63bp-2, -0x1.b5d1009e15ccp-2, -0x1.87de2a6aea963p-2, -0x1.58f9a75ab1fddp-2, -0x1.294062ed59f06p-2,
+    -0x1.f19f97b215f1bp-3, -0x1.8f8b83c69a60bp-3, -0x1.2c8106e8e613ap-3, -0x1.917a6bc29b42cp-4, -0x1.91f65f10dd814p-5};
 
-  int32_t k = (uint32_t)m << (31 - s);
-  double z = k, z2 = z * z;
-  double fs = SN[0] + z2 * (SN[1] + z2 * SN[2]);
-  double fc = CN[0] + z2 * (CN[1] + z2 * CN[2]);
-  uint32_t iq = m >> s;
-  iq = (iq + 1) >> 1;
-  uint32_t is = iq & 127, ic = (iq + 32) & 127;
+  b32u32_u ix = {.f = x};
+  int32_t e = (ix.u>>23)&0xff;
+  if(__builtin_expect(e == 0xff, 0)){
+    if(!(ix.u << 9)){
+#ifdef CORE_MATH_SUPPORT_ERRNO
+      errno = EDOM;
+#endif
+      feraiseexcept (FE_INVALID);
+      return __builtin_nanf("inf");
+    }
+    return x + x; // nan
+  }
+  int32_t m = (ix.u&~0u>>9)|1<<23, sgn = ix.u; sgn >>= 31;
+  m = (m^sgn) - sgn;
+  int32_t s = 143 - e;
+  if(__builtin_expect(s<0, 0)){ // |x| >= 0x1p+17
+    if(__builtin_expect(s<-6, 0)) // |x| >= 0x1p+23
+      return __builtin_copysignf(0.0f, x);
+    int32_t iq = (uint32_t)m<<(-s-1);
+    iq &= 127;
+    if(iq==0||iq==64) return __builtin_copysignf(0.0f, x);
+    return S[iq];
+  } else if(__builtin_expect(s>30, 0)){ // |x| < 0x1p-14
+    double z = x, z2 = z*z;
+#ifdef CORE_MATH_SUPPORT_ERRNO
+  /* For |x| <= 0x1.45f3p-128, sinpi(x) underflows whatever the rounding mode,
+     and for |x| >= nextabove(0x1.45f3p-128) = 0x1.45f308p-128, sinpi(x) does
+     not underflow whatever the rounding mode. */
+    if (x != 0 && __builtin_fabsf (x) <= 0x1.45f3p-128f)
+      errno = ERANGE; // underflow
+#endif
+    return z*(0x1.921fb54442d18p+1 + z2*(-0x1.4abbce625be53p+2));
+  }
+  int32_t si = 25 - s;
+  if(__builtin_expect(si>=0&&((uint32_t)m<<si)==0,0)) return __builtin_copysignf(0.0f, x);
+
+  int32_t k = (uint32_t)m<<(31-s);
+  double z = k, z2 = z*z;
+  double fs = sn[0] + z2*(sn[1] + z2*sn[2]);
+  double fc = cn[0] + z2*(cn[1] + z2*cn[2]);
+  uint32_t iq = m>>s; iq = (iq + 1)>>1;
+  uint32_t is = iq&127, ic = (iq + 32)&127;
   double ts = S[is], tc = S[ic];
-  double r = ts + (ts * z2) * fc + (tc * z) * fs;
+  double r = ts + (ts*z2)*fc + (tc*z)*fs;
   return r;
 }
 libm_alias_float (__sinpi, sinpi)
